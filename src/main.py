d2ecef438dabdd121739a4b61e22d0089a8b578c
N.T.WORKS <nagisc007@yahoo.co.jp>
2019-12-20 08:52:07 +0900

    Added: doc/titles (#17)

--- src/main.py
+++ src/main.py
@@ -10,11 +10,34 @@ from storybuilder.common.times import COMMON_TIMES
 from config import PERSONS, CHARAS, STAGES, DAYS, TIMES, ITEMS, WORDS
 from src.demo.main import ep_demo
 
+## NOTE:
+#   名作10本
+#   1. 走れメロス（太宰治）
+#   1. 吾輩は猫である（夏目漱石）
+#   1. 雪国（川端康成）
+#   1. 蜘蛛の糸（芥川龍之介）
+#   1. 金閣寺（三島由紀夫）
+#   1. 城の崎にて（志賀直哉）
+#   1. こころ（夏目漱石）
+#   1. 檸檬（梶井基次郎）
+#   1. 人間失格（太宰治）
+#   1. 銀河鉄道の夜（宮沢賢治）
+
 
 ## main
 def ch_main(w: World):
     return w.chapter("main",
-            ep_demo(w),
+            ep_demo(w).omit(),
+            # 1.走れ編集者
+            # 1.我輩は編集者である
+            # 1.出張先は雪国
+            # 1.蜘蛛よりも細い糸
+            # 1.金閣寺と矜持
+            # 1.城の崎に行きたい
+            # 1.作家のこころ
+            # 1.檸檬は嫌いです
+            # 1.編集失格
+            # 1.銀河鉄道の長い夜
             )
 
 def world():
